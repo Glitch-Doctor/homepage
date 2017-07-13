6fae08687488830494e17eb711f2c047e952c988
Glitch-Doctor <richard.tompsett@outlook.com>
2017-07-13 08:52:52 +0100

    Skeleton site upload

---
+++ Controllers/Web/AppController.cs
@@ -0,0 +1,66 @@
+﻿using Glitchdoctor.Models;
+using Glitchdoctor.Services;
+using Glitchdoctor.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace Glitchdoctor.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IConfigurationRoot _config;
+        private IMailService _mailService;
+        private PollContext _context;
+
+        public AppController(IConfigurationRoot config, IMailService mailService, PollContext context)
+        {
+            _config = config;
+            _mailService = mailService;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var recentPolls = _context.Polls.ToList();
+
+            return View(recentPolls);
+        }
+
+        public IActionResult MainAccounts()
+        {
+            return View();
+        }
+
+        public IActionResult Poll()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], vm.Email, "From TestSite", vm.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Debug;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Glitchdoctor.Services;
+using Glitchdoctor.Models;
+using Glitchdoctor.ViewModels;
+using System;
+using System.Linq;
+
+namespace Glitchdoctor
+{
+    public class Startup
+    {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_config);
+
+            if (_env.IsEnvironment("Development") || _env.IsEnvironment("Testing"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                // Remember to implement a real mail service
+            }
+
+            services.AddLogging();
+
+            services.AddDbContext<PollContext>();
+
+            services.AddMvc(config =>
+            {
+                if (_env.IsProduction())
+                {
+                    config.Filters.Add(new RequireHttpsAttribute());
+                }
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+           // app.UseIdentity();
+
+            loggerFactory.AddConsole();
+
+            if (_env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                loggerFactory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                loggerFactory.AddDebug(LogLevel.Error);
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" });
+            });
+
+        }
+    }
+}
