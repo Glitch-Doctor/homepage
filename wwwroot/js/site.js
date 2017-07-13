124a9def31f1e3b04dc3c2f29df6d192d49dbf8e
Glitch-Doctor <richard.tompsett@outlook.com>
2017-07-13 08:58:47 +0100

    Skeleton site upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿/* site.js */
+
+  (function () {
+
+    var ele = $("#username");
+    ele.text("Glitch Doctor");
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper,.navbar");
+    var $icon = $("#sidebarToggle i.fa");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-double-left");
+            $icon.addClass("fa-angle-double-right");
+            $("#title").text("Show Menu");
+        } else {
+            $icon.removeClass("fa-angle-double-right");
+            $icon.addClass("fa-angle-double-left");
+            $("#title").text("Hide Menu");
+        }
+    });
+
+  })();
